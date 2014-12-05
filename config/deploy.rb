f4603727761c003b76533b18dd1e0176724d264c
fx poignart <fx@gazelle>
2014-12-05 15:27:33 +0400

    make sure deploy the app is restarted after deploy symlink locales dir

--- config/deploy.rb
+++ config/deploy.rb
@@ -13,6 +13,8 @@ set :scm, :git
 # set :linked_files, %w{config/database.yml}
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+set :linked_dirs, fetch(:linked_dirs, []).push(%w{locales})
+
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 # set :keep_releases, 5
 
@@ -21,8 +23,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
@@ -35,6 +36,7 @@ namespace :deploy do
     end
   end
 
+  after :publishing, :restart
   after :finishing, 'deploy:cleanup'
 
 end
