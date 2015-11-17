6bdae5bced369ee6abc235cdd4f05b84654dceb5
fx poignart <fx@gazelle>
2015-11-17 17:49:30 +0400

    added production_de stage to allow site for german translation

--- config/deploy/production_de.rb
+++ config/deploy/production_de.rb
@@ -1,13 +1,15 @@
-set :stage, :staging
+set :stage, :production_de
+set :deploy_to, '/var/www/pyx4.com/translate_de'
+
 
 # Simple Role Syntax
 # ==================
 # Supports bulk-adding hosts to roles, the primary
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
-role :app, %w{deploy@example.com}
-role :web, %w{deploy@example.com}
-role :db,  %w{deploy@example.com}
+role :app, %w{deployer@prod1.pyx4.com}
+role :web, %w{deployer@prod1.pyx4.com}
+role :db,  %w{deployer@prod1.pyx4.com}
 
 # Extended Server Syntax
 # ======================
@@ -15,7 +17,7 @@ role :db,  %w{deploy@example.com}
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
+server 'prod1.pyx4.com', user: 'deployer', roles: %w{web app}
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
@@ -39,4 +41,4 @@ server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 #   }
 # setting per server overrides global ssh_options
 
-# fetch(:default_env).merge!(rails_env: :staging)
+# fetch(:default_env).merge!(rails_env: :production)
